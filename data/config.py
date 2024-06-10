60a562256e81c19a5b14a118e16246ea89928915
Abbos1308 <124450651+Abbos1308@users.noreply.github.com>
2024-06-10 13:40:46 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -9,5 +9,8 @@ env.read_env()
 BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
 ADMINS = env.list("ADMINS")  # adminlar ro'yxati
 IP = env.str("ip")  # Xosting ip manzili
-
+POSTGRES_USER = env.str("POSTGRES_USER")
+POSTGRES_PASSWORD = env.str("POSTGRES_PASSWORD")
+POSTGRES_DB = env.str("POSTGRES_DB")
+POSTGRES_HOST = env.str("POSTGRES_HOST")
 
