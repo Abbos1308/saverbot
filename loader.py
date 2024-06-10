79ad6bb973057d3ba7d657b8f828cd4aa2580a91
Abbos1308 <124450651+Abbos1308@users.noreply.github.com>
2024-06-10 10:05:42 +0500

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,9 +1,10 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from utils.db_api.sqlite import Database
-from data import config
+from data.config import *
 
-bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
+bot = Bot(token=BOT_TOKEN, parse_mode=types.ParseMode.HTML)
+
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
-db = Database(dbname="saverbot_db", user="saverbot_db_user", password="QgLON8WtRCH5WZIcrLgZkme0EFZCumtv", host="dpg-cpj7926ct0pc7384ua1g-a.oregon-postgres.render.com", port="5432")
+db = Database(dbname=POSTGRES_DB, user=POSTGRES_USER, password=POSTGRES_PASSWORD, host=POSTGRES_HOST, port="5432")
