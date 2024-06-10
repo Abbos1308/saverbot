455e6a84aedd5103f4809bd1abf865b4c755921c
Abbos1308 <124450651+Abbos1308@users.noreply.github.com>
2024-06-10 09:54:11 +0500

    Update loader.py

--- loader.py
+++ loader.py
@@ -6,4 +6,4 @@ from data import config
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
-db = Database(path_to_db="main.db")
+db = Database(dbname="saverbot_db", user="saverbot_db_user", password="QgLON8WtRCH5WZIcrLgZkme0EFZCumtv", host="dpg-cpj7926ct0pc7384ua1g-a.oregon-postgres.render.com", port="5432")
