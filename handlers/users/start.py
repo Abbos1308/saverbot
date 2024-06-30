66758f8278ea3dc7608062c9e06a4169d21a4375
Abbos1308 <124450651+Abbos1308@users.noreply.github.com>
2024-06-30 14:23:31 +0500

    Update start.py

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -49,8 +49,9 @@ async def bot_start(message: types.Message):
     except:
         pass
     user_id = message.from_user.first_name
-    await message.answer("🤖Bu bot orqali quyidagilarni yuklab olishingiz mumkin: \n• Instagram - (stories/post/reels) \n• TikTok - (video/photo) \n• YouTube - (video)\n• Facebook - (video)\n\n😉Maksimal yuklash hajmi - 400mb\n🤖 @full_downloaderr_bot")
-
+    await message.answer("salom")
+    #await message.answer("🤖Bu bot orqali quyidagilarni yuklab olishingiz mumkin: \n• Instagram - (stories/post/reels) \n• TikTok - (video/photo) \n• YouTube - (video)\n• Facebook - (video)\n\n😉Maksimal yuklash hajmi - 400mb\n🤖 @full_downloaderr_bot")
+    
     #await message.answer(f"Assalomu alaykum{user_id}\n\n </b>Media yuklashim uchun havolani yuboring")
 
 instagram_regex = r'(https?:\/\/(?:www\.)?instagram\.com\/[-a-zA-Z0-9@:%._+~#=]*)'
