aa07e67113c3db0ef23aad8ea5cfd891c5a626e1
Abbos1308 <124450651+Abbos1308@users.noreply.github.com>
2024-07-05 07:22:58 +0500

    Update download.py

--- handlers/users/download.py
+++ handlers/users/download.py
@@ -5,13 +5,13 @@ async def download_file(url, filename):
     try:
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as response:
-                if response.status == 200:
-                    data = await response.read()
-                    with open(filename, "wb") as f:
-                        f.write(data)
-                    print(f"Downloaded {filename} successfully!")
-                else:
-                    print(f"Error: Status code {response.status} received.")
+                #if response.status == 200:
+                data = await response.read()
+                with open(filename, "wb") as f:
+                    f.write(data)
+                print(f"Downloaded {filename} successfully!")
+                #else:
+                    #print(f"Error: Status code {response.status} received.")
     except Exception as e:
         print(f"An error occurred: {e}")
 
